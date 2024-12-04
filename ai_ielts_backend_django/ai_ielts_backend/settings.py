3fac780c4cd07c122ad2120c67473a1c247866e5
AntiZpvoh <wwwqyy@126.com>
2024-12-04 00:21:16 -0800

    add slash

--- ai_ielts_backend_django/ai_ielts_backend/settings.py
+++ ai_ielts_backend_django/ai_ielts_backend/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = '/var/www/html/static'
+STATIC_ROOT = '/var/www/html/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
